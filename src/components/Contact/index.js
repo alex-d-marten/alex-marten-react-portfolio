7c41e083bede62677087381c43200dd251a2ea65
amarten <alexdmarten@gmail.com>
2022-03-26 21:25:03 -0700

    added resume component without styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,19 +3,21 @@ import React from "react";
 function Contact() {
     return (
         <form>
-            <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Name</label>
-                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Enter your name here"/>
+            <div className="p-5 container">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label px-2">Name</label>
+                    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Enter your name here"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput2" className="form-label px-2">Email address</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput2" placeholder="name@example.com"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label px-2">Message</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Send me a message!" rows="7"></textarea>
+                </div>
+                <button className="btn btn-primary" type="submit">Submit</button>
             </div>
-            <div className="mb-3">
-                <label htmlFor="exampleFormControlInput2" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleFormControlInput2" placeholder="name@example.com"/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-            <button className="btn btn-primary" type="submit">Submit</button>
         </form>
     )
 }
---
+++ src/components/Resume/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import resumePdf from '../../assets/files/Alex_Marten_Web_Developer_Resume.pdf';
+
+function Resume() {
+    return (
+        <div className="resume-body">
+            <h2 className="p-3">My Resume</h2>
+            <div className="resume-download">
+                <h3 className="p-3">
+                    Download my 
+                    <a href={resumePdf} target="_blank" rel="noopener noreferrer"> Resume</a>
+                </h3>
+            </div>
+            <div className="resume-text p-3">
+                <div className="front-end">
+                    <h3>Front-End Experience</h3>
+                    <ul>
+                        <li>JavaScript</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JQuery</li>
+                        <li>React</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </div>
+                <div className="back-end">
+                    <h3>Back-End Experience</h3>
+                    <ul>
+                        <li>APIs</li>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                        <li>MySQL</li>
+                        <li>Sequelize</li>
+                        <li>NoSQL</li>
+                        <li>MongoDB</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Resume;
