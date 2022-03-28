cc6e64fbd1c7dca90cfeb09e945b9dbd74d1bae5
amarten <alexdmarten@gmail.com>
2022-03-27 17:42:06 -0700

    updated about image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import avatarImage from '../../assets/images/avatar-placeholder.png'
+import avatarImage from '../../assets/images/selfie.jpg'
 
 function About() {
     return (
@@ -7,13 +7,13 @@ function About() {
             <div>
                 <h1 className="py-3">About Me</h1>
                 {/* include a picture of an avatar/me here */}
-                <img src={avatarImage} alt="avatar" style={{ width: "100px"}} className="my-5"/>
+                <img src={avatarImage} alt="avatar" style={{ width: "400px"}} className="my-5"/>
                 <h3>Introduction</h3>
-                <p className="py-3">My name is Alex Marten and welcome to my React Portfolio. The above navigation bar can take you to different sections of my portfolio to view my current projects, how to contact me and to view my resume. Take a look around and if you ahve any questions please contact me in the contact section.</p>
-            </div>
-            <div>
-                <h3>Hobbies</h3>
-                <p className="py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dictum arcu. Vivamus id lorem nulla. Morbi ultricies dui rhoncus est tristique, non finibus odio rutrum. Integer a mollis lorem. Praesent vel varius dolor. Aliquam libero nulla, egestas in risus.</p>
+                <p className="py-3">My name is Alex Marten and welcome to my React Portfolio! I am an aspiring web developer and am looking to portray some of the projects I have completed during the UC Berkely Bootcamp. This portfolio is a single page application and this has been accomplished with React.
+                <br />
+                <br />
+                The above navigation bar can take you to different sections of my portfolio to view my current projects, how to contact me and to view my resume. Take a look around and if you have any questions please contact me in the contact section.
+                </p>
             </div>
         </section>
     )
