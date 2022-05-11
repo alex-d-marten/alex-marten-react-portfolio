37fbd4842ab24f3f64f2f2493a278f784f5f2b95
amarten <alexdmarten@gmail.com>
2022-05-11 08:23:50 -0700

    updated the contact page for collapsable buttons

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -31,29 +31,57 @@ function Contact() {
         alert('This form is in progress, please email me at `alexdmarten@gmail.com` or contact me on LinkedIn');
     }
 
+    const [showShow, setShowShow] = useState(false);
+
+    const toggleShow = () => setShowShow(!showShow);
+
+
     return (
-        <form id="contact-form" onSubmit={handleSubmit}>
-            <div className="p-5 container">
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label px-2">Name</label>
-                    <input type="text" className="form-control" name="name" defaultValue={name} onBlur={handleChange}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label px-2">Email address</label>
-                    <input type="email" className="form-control" name="email" defaultValue={email} onBlur={handleChange}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="message" className="form-label px-2">Message</label>
-                    <textarea className="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="7"></textarea>
-                </div>
-                {errorMessage && (
-                    <div>
-                        <p className="error-text">{errorMessage}</p>
+        <div className="container m-5">
+            <div className="row justify-content-between">
+                <div className="col-4">
+                    <a className="btn btn-primary" data-mdb-toggle="collapse" href="#emailCollapse" role="button" aria-expanded="false" aria-controls="emailCollapse">Email</a>
+                    <div className="collapse mt-3" id="emailCollapse">
+                        alexdmarten@gmail.com
                     </div>
-                )}
-                <button className="btn btn-primary" type="submit">Submit</button>
+                </div>
+                <div className="col-4">
+                    <a className="btn btn-primary" data-mdb-toggle="collapse" href="#phoneCollapse" role="button" aria-expanded="false" aria-controls="phoneCollapse">Phone</a>
+                    <div className="collapse mt-3" id="phoneCollapse">
+                        916-303-6073
+                    </div>
+                </div>
+                <div className="col-4">
+                    <a className="btn btn-primary" data-mdb-toggle="collapse" href="#liCollapse" role="button" aria-expanded="false" aria-controls="liCollapse">LinkedIn</a>
+                    <div className="collapse mt-3" id="liCollapse">
+                    www.linkedin.com/in/admarten
+                    </div>
+                </div>
             </div>
-        </form>
+
+            {/* <form id="contact-form" onSubmit={handleSubmit}>
+                <div className="p-5 container">
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label px-2">Name</label>
+                        <input type="text" className="form-control" name="name" defaultValue={name} onBlur={handleChange}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label px-2">Email address</label>
+                        <input type="email" className="form-control" name="email" defaultValue={email} onBlur={handleChange}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="message" className="form-label px-2">Message</label>
+                        <textarea className="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="7"></textarea>
+                    </div>
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )}
+                    <button className="btn btn-primary" type="submit">Submit</button>
+                </div>
+            </form> */}
+        </div>
     )
 }
 
